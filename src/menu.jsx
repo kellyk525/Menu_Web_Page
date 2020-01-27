a8da0fc9407bcca9ea1dbb3ccb712f1c46d42826
Kelly <kjk224@cornell.edu>
2020-01-27 10:49:17 -0800

    created menu items

--- src/menu.jsx
+++ src/menu.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Data from './data';
+import MenuItem from './menu_item';
 
 class Menu extends React.Component {
     constructor(props) {
@@ -26,12 +27,18 @@ class Menu extends React.Component {
     render() {
 
         const { menu } = this.state;
-        debugger
-        const { category } = menu[0].brands[0].storeGroups[0].stores[0].menus[0].campaignMenuAudits[0].items[0].categories.title;
+        const category = menu[0].brands[0].storeGroups[0].stores[0].menus[0].campaignMenuAudits[0].items[0].categories.title;
+        const list = menu[0].brands[0].storeGroups[0].stores[0].menus[0].campaignMenuAudits[0].items
 
+        const appetizer = list.map( (item, i) => {
+            return (
+                <li key={ i }><MenuItem item={item} /></li>
+            )
+        })
         return (
             <div>
-                Hello Menu
+                <div>{ category }</div>
+                <ul>{ appetizer }</ul>
             </div>
         )
     }
--- src/menu_item.jsx
+++ src/menu_item.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
 
-class React extends React.Component {
+class MenuItem extends React.Component {
     constructor(props) {
         super(props);
-        
+    }
+
+    render() {
+        const { item } = this.props;
+
+        if (!item) {
+            return null;
+        }
+
+        return (
+            <div>
+                { item.title }
+            </div>
+        )
     }
 }
+
+export default MenuItem;
